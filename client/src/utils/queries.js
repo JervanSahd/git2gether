222720087faa4ba806f0d44e0103a4b2ae5850ca
autumnlf <autumnF12@gmail.com>
2022-06-02 13:33:32 -0400

    debugging queries and mutations

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,17 +1,77 @@
 import { gql } from '@apollo/client';
 
-export const GET_inPerson = gql`
-  queries getinPerson($_id: ID!) {
-    getinPerson(ID!: $_id) {
-      }
+export const QUERY_INPERSON = gql`
+  query getInPersonEvents {
+    InPersonAll {
+      _id
+      name
+      location
+      startDate
+      endDate
+      startTime
+      endTime
+      description
+      plannerName
+      email
+      phone
+      linkedIn
     }
   }
 `;
 
-export const GET_virtual= gql`
-  queries getVirtual($_id: ID!) {
-    getVirtual(ID!: $_id) {
-      }
+export const QUERY_VIRTUAL = gql`
+  query getVirtualEvents {
+    virtualAll {
+      _id
+      name
+      streamLink
+      startDate
+      endDate
+      startTime
+      endTime
+      description
+      plannerName
+      email
+      phone
+      linkedIn
+    }
+  }
+`;
+
+export const QUERY_SINGLE_INPERSON = gql`
+  query getSingleInPerson($eventId: ID!) {
+    inPerson(eventId: $eventId) {
+      _id
+      name
+      location
+      startDate
+      endDate
+      startTime
+      endTime
+      description
+      plannerName
+      email
+      phone
+      linkedIn
+    }
+  }
+`;
+
+export const QUERY_SINGLE_VIRTUAL = gql`
+  query getSingleVirtual($_id: ID!) {
+    virtual(eventId: $eventId) {
+      _id
+      name
+      streamLink
+      startDate
+      endDate
+      startTime
+      endTime
+      description
+      plannerName
+      email
+      phone
+      linkedIn
     }
   }
 `;
